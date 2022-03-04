fafaaec793c8b530b80723e3fdd3957be0dedd70
dhesinguraja <rajadhesing3@gmail.com>
2022-03-04 11:01:03 +0530

    file changes done

--- Employee_Management_System/src/main/java/com/employee/model/EmployeeOperation.java
+++ Employee_Management_System/src/main/java/com/employee/model/EmployeeOperation.java
@@ -1,5 +1,43 @@
 package com.employee.model;
 
-public class EmployeeOperation {
+import java.util.ArrayList;
+import java.util.List;
 
+public class EmployeeOperation {
+	
+	public List<Double> salaryOperation(double Bp) {
+		Double DA=Bp*0.4;
+		Double HRA=Bp*0.3;
+		Double Gross=Bp+DA+HRA;
+		
+		Double Tax=(double) 0;
+		if(Gross>15000000)
+		{
+			Tax=Gross*0.3;
+		}
+		else if (Gross>1000000)
+			
+		{
+			Tax=Gross*0.2;
+		}
+        else if (Gross>500000)
+			
+		{
+			Tax=Gross*0.1;
+		}
+        else
+        {
+			Tax=(double) 0;
+			
+		}
+		Double Net=Gross-Tax;
+		
+		List<Double> res = new ArrayList<Double>();
+		res.add(DA);
+		res.add(HRA);
+		res.add(Gross);
+		res.add(Tax);
+		res.add(Net);
+		return res;
+	}
 }
--- Employee_Management_System/src/main/java/com/employee/service/EmployeeService.java
+++ Employee_Management_System/src/main/java/com/employee/service/EmployeeService.java
@@ -1,5 +1,41 @@
 package com.employee.service;
 
-public class EmployeeService {
+import java.util.List;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+ 
+import com.employee.doa.EmployeeDAO;
+import com.employee.model.Employee;
+
+@Path("/employeeService")
+public class EmployeeService{
+	@GET
+	@Path("/displayemployee")
+	@Produces("text/plain")
+	public static Employee getEmployeeDetails(int id) {
+		EmployeeDAO empDao= new EmployeeDAO();
+		List<Employee> empList= empDao.findById(id);
+		//System.out.println(empList.get(0));
+		return empList.get(0);
+	}
+	
+	@GET
+	@Path("/displayemployee")
+	@Produces("text/plain")
+	public static List<Employee> getAllEmployee() {
+		EmployeeDAO empDao= new EmployeeDAO();
+		List<Employee> empList= empDao.getAllEmployee();
+		return empList;
+	}
+	
+	@GET
+	@Path("/displayemployee")
+	@Produces("text/plain")
+	public static void addEmployee(Employee e) {
+		EmployeeDAO empDao= new EmployeeDAO();
+		empDao.saveEmployee(e);
+	}
 }
