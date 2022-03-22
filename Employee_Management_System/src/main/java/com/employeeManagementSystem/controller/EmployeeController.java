adffb3699c0f4aa5301866e5309a7a3e50158e5a
dhesinguraja <rajadhesing3@gmail.com>
2022-03-22 16:29:36 +0530

    done

--- Employee_Management_System/src/main/java/com/employeeManagementSystem/controller/EmployeeController.java
+++ Employee_Management_System/src/main/java/com/employeeManagementSystem/controller/EmployeeController.java
@@ -11,10 +11,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,6 +33,8 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/portal")
 @RestController
 @Slf4j
+@CrossOrigin(origins = "http://localhost:4200")
+
 public class EmployeeController {
 	
 	@Autowired
@@ -137,6 +141,11 @@ public class EmployeeController {
 		model.setViewName("registerUser");
 		return model;
 	}
+	@PostMapping(path = "/registerUser", consumes = "application/json")
+    public void addUser(@RequestBody User user) {
+		employeeService.addUser(user);
+    
+    }
 	
 	@RequestMapping(value = "/newUser", method = RequestMethod.GET)
 	public ModelAndView addUser(HttpServletRequest request,ModelAndView model) {
@@ -159,5 +168,10 @@ public class EmployeeController {
 			model.setViewName("fail");
 			return model;
 		}
+	}
+	
+	@GetMapping(path="/allUserList",produces="application/json")
+	public List<User> getAllUser(){
+		return employeeService.allUser();
 	}
 }
