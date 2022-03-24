a88af9fc834d7ed8a87964749f99926474cdb9ec
dhesinguraja <61825150+dhesinguraja@users.noreply.github.com>
2022-03-25 05:24:05 +0530

    changes file

--- Emp__Table_Project.sql
+++ Emp__Table_Project.sql
@@ -1,18 +1,21 @@
-create table user_details( USER_ID    NOT NULL VARCHAR2(255 CHAR) ,
-ADDRESS             VARCHAR2(255 CHAR) ,
-EMAIL               VARCHAR2(255 CHAR) ,
-FIRST_NAME          VARCHAR2(255 CHAR) ,
-LAST_NAME           VARCHAR2(255 CHAR) ,
-PASSWORD            VARCHAR2(255 CHAR) ,
-EMP_ID              NUMBER(10));
+ create table "user"(
+    userid varchar2(255) not null,
+    address varchar2(255),
+    email varchar2(255),
+    first varchar2(255),
+    last varchar2(255),
+    password varchar2(255),
+    phone number(19),
+    primary key (userid)
+);
  
-select * from user_details;
-desc user_details;
-insert into user_details(user_Id,password,first_Name,last_Name,email,address,emp_id) values ('1','raja','raja','roy','abc@gmail.com','chennai',1);
-insert into user_details(user_Id,password,first_Name,last_Name,email,address,emp_id) values ('3','akku','akku','roy','cdf@gmail.com','chennai',1);
+INSERT INTO "user"(userid,first,last,email,phone,password,address)
+VALUES(ib123,bittu,kumar,jackbabu661@gmail.com,878956,ib,abc);
+ 
+INSERT INTO "user"(userid,first,last,email,phone,password,address)
+VALUES(bittu,bittu,indian,bittu@gmail.com,878956,bittu,abc);
 
-
-create table Employee(ID NOT NULL NUMBER(10)  ,       
+create table Employee(ID  NUMBER(10)  NOT NULL  ,       
 BASIC_PAY                FLOAT(126)  ,       
 DA                       FLOAT(126),         
 DATE_OF_JOINING          VARCHAR2(255 CHAR) ,
@@ -20,9 +23,12 @@ GROSS                    FLOAT(126)  ,
 HRA                      FLOAT(126) ,        
 NAME                     VARCHAR2(255 CHAR) ,
 NET                      FLOAT(126)   ,  
-TAX                      FLOAT(126)  );
+TAX                      FLOAT(126),
+primary key (ID));
 insert into Employee(ID,NAME,DATE_OF_JOINING,BASIC_PAY,DA,HRA,GROSS,TAX,NET) values (55,'akku','2022-03-15',50000,20,1600,10000,0,10000);
 commit;
 
 select * from Employee;
 desc Employee;
+
+select * from "user";
